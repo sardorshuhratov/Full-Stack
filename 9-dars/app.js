db9a645d2e070c797e2c13fdd5d49200d1982757
sardorshuhratov <sardorbekshuhratov709@gmail.com>
2026-03-05 13:22:23 +0500

    New Lesson  9-dars

---
+++ 9-dars/app.js
@@ -0,0 +1,76 @@
+// // task 1
+// for (let i = 1; i < 20; i++) {
+//     if (i % 2 === 0) {
+//         continue;
+//     }
+//     console.log(i);
+// }
+
+// // task 2
+// let sum = 0, i = 1;
+// while (i <= 10) {
+//     sum += i;
+//     i++;
+// }
+// console.log("Sonlar yig'indisi: " + sum);
+
+// // task 3
+// let i = 10;
+// do {
+//     console.log(i);
+//     i--;
+// } while (i >= 1);
+
+// // task 4
+// for (let i = 1; i <= 50; i++) {
+//     if (i % 7 === 0) {
+//         console.log(i);
+//         break;
+//     }
+// }
+
+// // task 5.
+// for (i = 1; i <= 30; i++) {
+//     if (i % 3 === 0 && i % 5 === 0) {
+//         console.log(i);
+//     }
+// }
+
+// // task 6.
+// for (let i = 1; i <= 100; i++) {
+//     if (i === 50) {
+//         break;
+//     }
+//     if (i % 9 === 0) {
+//         console.log(i);
+//     }
+// }
+
+// // task 7.
+// const prompt = require('prompt-sync')();
+// let i;
+// do {
+//     i = Number(prompt("Son kiriting:"));
+//     if (i != 0) {
+//         console.log("Siz " + i + " sonini kiritdingiz.");
+//     }
+// } while (i !== 0);
+// console.log("Dastur tugadi.");
+
+// // task 8.
+// for (let i = 1; i <= 10; i++) {
+//     // Har bir sonning kvadratini hisoblab, konsolga chiqaramiz
+//     console.log(i ** 2);
+// }
+
+// // task 9.
+// for (let i = 1; i <= 100; i++) {
+//     if (i % 13 === 0) {
+//         console.log("Topildi: ");
+//         console.log(i);
+//         break;
+//     }
+// }
+
+// task 10.
+
