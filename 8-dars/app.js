0d94a9f4420286929ac2cebb89a6121e3f495cfe
sardorshuhratov <sardorbekshuhratov709@gmail.com>
2026-03-05 12:30:19 +0500

    Update 8-dars

--- 8-dars/app.js
+++ 8-dars/app.js
@@ -28,6 +28,8 @@
 //     console.log("Meva: " + fruits[i]);
 // }
 
+// While loop
+
 // // 6.
 // let i = 1;
 // while (i <=5) {
@@ -58,4 +60,55 @@
 // }
 // console.log("2 ning darajalari 25 dan katta bo'lganda to'xtaydi.");
 
-// 10.
+// Do While Loop
+
+// // 10.
+// let i = 1;
+// do {
+//     console.log("Son: " + i);
+//     i++;
+// } while (i <= 5);   
+
+// // 11.
+// let sum = 0, j = 1;
+// do {
+//     sum += j;
+//     j++;
+// } while (j <= 10);
+
+// contune va break
+
+// // 12.
+// for (let i = 1; i <= 5; i++) {
+//     if (i === 3) {
+//         break;
+//     }
+//     console.log("Son: " + i);
+//  // i 3 ga teng bo'lsa, keyingi kodni o'tkazib yuboradi
+// }
+
+// // 13
+// for (let i = 1; i <= 5; i++) {
+//     if (i === 3) {
+//         continue;
+//     }
+//     console.log("Son: " + i);
+// }
+
+// // 14.
+// let fruits = ["Olma", "Banan", "Apelsin", "Anor"];
+// for (let i = 0; i < fruits.length; i++) {
+//     if (fruits[i] === "Banan") {
+//         continue;
+//     }
+//     console.log("Meva: " + fruits[i]);  
+// }
+
+// // 15.
+// for (let i = 1; i <= 5; i++) {
+//     if (i % 2 === 0) {
+//     continue;
+//     }
+//     console.log("Toq son: " + i);
+// }
+
